d6d5e1d0c073fbe17d90b7e63a374516b804562e
Tony Boucheau <boucheau.tony@neuf.fr>
2017-01-30 15:40:42 +0100

    :except => [:index]

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user! only: [:new, :show, :create]
+  before_action :authenticate_user! :except => [:index]
 
 
   def new
