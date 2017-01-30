8d792d8621808054804ff7e8ffbe1ee214f01bcb
Tony Boucheau <boucheau.tony@neuf.fr>
2017-01-30 13:30:37 +0100

    route config

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-
-  get 'posts/index'
-
-  get 'posts/show'
-
+  root 'posts#index'
+  resources :posts
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
